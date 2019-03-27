ba85ffb468592e62c8182852a534ff26d8919647
Satish Babariya <satish.babariya@gmail.com>
2019-03-27 22:09:51 +0530

    Update configure.swift

--- Sources/Playground/configure.swift
+++ Sources/Playground/configure.swift
@@ -11,5 +11,14 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() 
     middlewares.use(FileMiddleware.self) 
     middlewares.use(ErrorMiddleware.self) 
+
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    middlewares.use(corsMiddleware)
+
     services.register(middlewares)
 }
